1e80a9a460b5d780dea6bc3bde8fc7930a345f14
Pablo Revuelta <pablo@catchdesign.co.nz>
2013-11-26 12:42:50 +1300

    Initial Commit

---
+++ GeckoMIDI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GeckoMIDI
+//
+//  Created by Pablo Revuelta on 26/11/13.
+//  Copyright (c) 2013 Midnight City. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ GeckoMIDI/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  GeckoMIDI
+//
+//  Created by Pablo Revuelta on 26/11/13.
+//  Copyright (c) 2013 Midnight City. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
